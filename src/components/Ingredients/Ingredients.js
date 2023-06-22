76975cbee69e02c849cac70f5b2036218d0506aa
Jan Golder <kontakt.jangolder@gmail.com>
2023-06-22 18:30:10 +0200

    add fetch post to database

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -8,7 +8,18 @@ function Ingredients() {
 const [userIngredients,setUserIngredients] = useState([]);
 
 const addIngredientHandler = ingredient =>{
-  setUserIngredients(prevIngredients => [...prevIngredients,{id: Math.random().toString(),...ingredient}]);
+  fetch('https://react-todo-8171c-default-rtdb.firebaseio.com/ingredients.json',{
+    method: 'POST',
+    body:JSON.stringify(ingredient),
+    headers: {'Content-Type':'application/json'}
+  }).then(response=>{
+    return response.json();
+  }).then(responseData =>{
+    setUserIngredients(prevIngredients => [...prevIngredients,{id: responseData.name,...ingredient}]);    
+  }
+  )
+
+
 }
 
 const removeIngredientHandler = ingredientId =>{
