ed6e57adbe040071a301afce7ac06d9379871e60
Jan Golder <kontakt.jangolder@gmail.com>
2023-06-23 18:33:16 +0200

    add fetch GET data with useEffect

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import IngredientList from './IngredientList';
 import IngredientForm from './IngredientForm';
 import Search from './Search';
@@ -6,6 +6,22 @@ import Search from './Search';
 function Ingredients() {
 
 const [userIngredients,setUserIngredients] = useState([]);
+
+useEffect(()=>{
+  fetch('https://react-todo-8171c-default-rtdb.firebaseio.com/ingredients.json')
+  .then(response => response.json())
+  .then(responseData=>{
+    const loadedIngredients = [];
+    for(const key in responseData){
+      loadedIngredients.push({
+        id: key,
+        title: responseData[key].title,
+        amount: responseData[key].amount
+      });
+    };
+    setUserIngredients(loadedIngredients);
+  })
+},[]);
 
 const addIngredientHandler = ingredient =>{
   fetch('https://react-todo-8171c-default-rtdb.firebaseio.com/ingredients.json',{
